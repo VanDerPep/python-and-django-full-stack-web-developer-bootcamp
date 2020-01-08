049f0bfddf845a685543eda72b4dcfe57c174b51
Shorokhov <oleksandr.shorokhov@ger.win.int.kn>
2020-01-08 16:10:11 +0200

    dom

---
+++ dom/part4_own.js
@@ -0,0 +1,24 @@
+var restart = document.querySelector("#b")
+var squares = document.querySelectorAll("td")
+
+function clearBoard() {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].textContent = ""
+    }
+}
+
+function changeMarker(){
+    if (this.textContent === ""){
+        this.textContent = "X"
+    }else if (this.textContent === "X") {
+        this.textContent = "O"
+    }else {
+        this.textContent = ""
+    }
+}
+
+restart.addEventListener('click', clearBoard)
+
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("click", changeMarker)
+}
